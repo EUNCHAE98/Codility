4306a5fbf5c26f77c0650ce9152305d88a1eeff8
EUNCHAE98 <tie0510@naver.com>
2019-01-31 20:18:16 +0900

    CyclicRotation

--- src/codility/CyclicRotation.java
+++ src/codility/CyclicRotation.java
@@ -2,23 +2,20 @@ package codility;
 // you can also use imports, for example:
 import java.util.*;
 
-public class CyclicRotation {
-
 	// you can write to stdout for debugging purposes, e.g.
 	// System.out.println("this is a debug message");
 
-	class Solution {
-	    public int[] solution(int[] A, int K) {
-	        // write your code in Java SE 8
-	        
-	        int arraySize = A.length;
-	        int result[] = new int[A.length];
-	        
-	        for(int i = 0; i < arraySize; i++){
-	            result[(i+K)%arraySize] = A[i];    
-	        }
-	        return result;
-	        
-	    }
-	}
+class Solution {
+    public int[] solution(int[] A, int K) {
+        // write your code in Java SE 8
+        
+        int arraySize = A.length;
+        int result[] = new int[A.length];
+        
+        for(int i = 0; i < arraySize; i++){
+            result[(i+K)%arraySize] = A[i];    
+        }
+        return result;
+        
+    }
 }
